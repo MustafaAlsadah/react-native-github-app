b474245507fe55aba9b6c85da8004a7de465256e
Mustafa Alsadah <sadah9999@gmail.com>
2023-06-14 16:09:57 +0300

    l

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,8 @@
 const { onRequest } = require("firebase-functions/v2/https");
 const fetch = require('node-fetch');
 const {logger} = require("firebase-functions");
+const { Configuration, OpenAIApi } = require("openai");
+// const axios = require('axios');
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -56,8 +58,72 @@ exports.repos = onRequest(async (req, res)=>{
       res.json(data);
 });
 
-exports.webhooks = onRequest((req, res)=>{
-    console.log("webhooks: "+JSON.stringify(req.body));
-    logger.log("webhooks: "+JSON.stringify(req.body));
-    res.json({message: "webhooks"});
+exports.ghWebHooks = onRequest(async (req, res)=>{
+
+console.log("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+console.log(req.body.issue.title);
+console.log(req.body.issue.comments_url);
+console.log(req.body.comment.user.login)
+console.log(req.body.comment.body)
+console.log("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFF FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+
+  
+const { title, body, comments_url } = req?.body?.issue;
+const { login } = req?.body?.comment.user;
+const { comment } = req?.body?.comment?.body;
+
+
+const postComment = async () => {
+
+  try {
+    const response = await fetch(
+      comments_url
+      ,{
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer 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`,
+          Accept: 'application/vnd.github.v3+json',
+        },
+        body: JSON.stringify({login: login, comment: comment}), // The content of the comment
+        body: JSON.stringify({ body: login+comment }), // The content of the comment
+      }
+    );
+
+    const data = await response.json();
+    console.log(JSON.stringify(data));
+
+  } catch (error) {
+    console.log('Error posting comment:', error);
+  }
+};
+
+await postComment();
+  
+    
+    res.json({message: "gptResponse"});
 });
+
+
+
+exports.gptClient = onRequest(async (req, res)=>{
+
+  const configuration = new Configuration({
+    apiKey: "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",
+  });
+  const openai = new OpenAIApi(configuration);
+  const prompt = `
+  This is an issue on github, please propose a solution for it:
+  
+  Explain how the world is running like i'm 5`;
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: prompt}]
+  });
+
+  const gptResponse = JSON.stringify(response?.data?.choices[0]?.message?.content ?? "UNEXPECTED ERROR: NO RESPONSE");
+
+  logger.log("GPT Response: "+gptResponse);
+  res.json({message: gptResponse});
+
+});
+ 
